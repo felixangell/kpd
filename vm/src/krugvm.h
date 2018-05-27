2d51a69f8592a94cab90757ff7a99921b801a29f
Felix Angell <felixangell@members.fsf.org>
2018-05-27 15:49:13 +0100

    remove vm for now in favour of llvm jit

--- vm/src/krugvm.h
+++
@@ -1,6 +0,0 @@
-#ifndef KRUGVM_H
-#define KRUGVM_H
-
-bool execute_program(size_t entry_addr, size_t instruction_count, unsigned char* program);
-
-#endif
--- vm/src/opcode_names.h
+++
@@ -1,6 +0,0 @@
-#ifndef OPCODE_NAMES_H
-#define OPCODE_NAMES_H
-
-extern const char* OPCODE_NAMES[];
-
-#endif
